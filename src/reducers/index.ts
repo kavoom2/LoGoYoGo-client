bcbfd0eba6ce26da4dc389e7154819bf41c18d44
yhl88 <yhl898989@gmail.com>
2021-04-06 16:35:37 +0900

    Add #5 - edit Authorization

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import loginReducer from "./loginReducer";
 import accessTokenReducer from "./accessTokenReducer";
+import userInfo from "./userInfoReducer";
 
 const rootReducer = combineReducers({
   loginReducer,
   accessTokenReducer,
+  userInfo,
 });
 
 export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
