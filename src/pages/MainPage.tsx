ccb539c7d260e6e85bf47bfce117a64d3d0204eb
yhl88 <yhl898989@gmail.com>
2021-04-06 16:37:19 +0900

    Add #5 - edit Authorization

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,27 @@
 import Nav from "../components/Nav";
 import LandingPart1 from "../components/LandingPart1";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { setLoginStatus, setUserInfo, setAccessToken } from "../actions/index";
 import "../scss/MainPage.scss";
 import "../scss/_CommonComponents.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../reducers";
+
+require("dotenv").config();
 
 export default function MainPage() {
+  const dispatch = useDispatch();
+
+  const loginState = useSelector((state: RootState) => state.loginReducer);
+  const isLogin = loginState.isLogin;
+
+  const handleSignout = (): void => {
+    dispatch(setUserInfo("", ""));
+    dispatch(setLoginStatus());
+    dispatch(setAccessToken(""));
+  };
+
   return (
     <div id="container-mainpage">
       <Nav />
