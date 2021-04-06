8ba6b58d9582d9c845ae3c7f752ec10307849d81
yhl88 <yhl898989@gmail.com>
2021-04-06 16:36:48 +0900

    Add #5 - edit Authorization

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,28 @@ import NotFound from "./pages/NotFound";
 import MainPage from "./pages/MainPage";
 import Maker from "./pages/Maker";
 import Profile from "./pages/Profile";
+//모달 구현에 앞서서 렌더링 테스트
+import SignUp from "./components/SignUp";
+import Signin from "./components/Signin";
+
 import "./css/App.css";
+import { useSelector } from "react-redux";
+import { RootState } from "./reducers";
 
 function App() {
+  const loginState = useSelector((state: RootState) => state.loginReducer);
+  const isLogin = loginState.isLogin;
+
+  console.log(isLogin);
+
   return (
     <Router>
       <Switch>
         <Route path="/" component={MainPage} exact />
         <Route path="/profile" component={Profile} />
         <Route path="/maker" component={Maker} />
+        <Route path="/signup" component={SignUp}></Route>
+        <Route path="/signin" component={Signin}></Route>
         <Route path="*" component={NotFound} />
       </Switch>
     </Router>
